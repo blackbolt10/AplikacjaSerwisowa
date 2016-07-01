ff1353191abf3c16c6dc2c79dbbc1b952e871d53
Krzysiek <k.w.03@wp.pl>
2016-07-01 13:28:31 +0200

    Generowanie plików KntKarty, KntAdresy, TwrKartySkładniki, TwrKartyCzynnosci

--- AplikacjaSerwisowaKomp/MainWindow.Designer.cs
+++ AplikacjaSerwisowaKomp/MainWindow.Designer.cs
@@ -32,43 +32,192 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.operatorzyPictureBox = new System.Windows.Forms.PictureBox();
+            this.twrKartySkladnikiPictureBox = new System.Windows.Forms.PictureBox();
+            this.twrKartyCzynnosciPictureBox = new System.Windows.Forms.PictureBox();
+            this.kntAdresyPictureBox = new System.Windows.Forms.PictureBox();
+            this.kntKartyPictureBox = new System.Windows.Forms.PictureBox();
+            this.operatorzyLabel = new System.Windows.Forms.Label();
+            this.twrKartySkladnikiLabel = new System.Windows.Forms.Label();
+            this.kntAdresyLabel = new System.Windows.Forms.Label();
+            this.twrKartyCzynnosciLabel = new System.Windows.Forms.Label();
+            this.kntKartyLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.operatorzyPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.twrKartySkladnikiPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.twrKartyCzynnosciPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kntAdresyPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kntKartyPictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(115, 32);
             this.button1.TabIndex = 0;
             this.button1.Text = "Operatorzy";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.operatorzyButton_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 41);
+            this.button2.Location = new System.Drawing.Point(12, 50);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(115, 32);
             this.button2.TabIndex = 1;
             this.button2.Text = "KntKarty";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.kntKartyButton_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(12, 70);
+            this.button3.Location = new System.Drawing.Point(12, 88);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(115, 32);
             this.button3.TabIndex = 2;
             this.button3.Text = "KntAdresy";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.Click += new System.EventHandler(this.kntAdresyButton_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 126);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(115, 32);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "TwrKartyCzynności";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.twrKartyCzynnosciButton_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(12, 164);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(115, 32);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "TwrKartySkladniki";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.twrKartySkladnikiButton_Click);
+            // 
+            // operatorzyPictureBox
+            // 
+            this.operatorzyPictureBox.BackColor = System.Drawing.Color.White;
+            this.operatorzyPictureBox.Enabled = false;
+            this.operatorzyPictureBox.Location = new System.Drawing.Point(142, 12);
+            this.operatorzyPictureBox.Name = "operatorzyPictureBox";
+            this.operatorzyPictureBox.Size = new System.Drawing.Size(32, 32);
+            this.operatorzyPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.operatorzyPictureBox.TabIndex = 5;
+            this.operatorzyPictureBox.TabStop = false;
+            // 
+            // twrKartySkladnikiPictureBox
+            // 
+            this.twrKartySkladnikiPictureBox.BackColor = System.Drawing.Color.White;
+            this.twrKartySkladnikiPictureBox.Enabled = false;
+            this.twrKartySkladnikiPictureBox.Location = new System.Drawing.Point(142, 164);
+            this.twrKartySkladnikiPictureBox.Name = "twrKartySkladnikiPictureBox";
+            this.twrKartySkladnikiPictureBox.Size = new System.Drawing.Size(32, 32);
+            this.twrKartySkladnikiPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.twrKartySkladnikiPictureBox.TabIndex = 6;
+            this.twrKartySkladnikiPictureBox.TabStop = false;
+            // 
+            // twrKartyCzynnosciPictureBox
+            // 
+            this.twrKartyCzynnosciPictureBox.BackColor = System.Drawing.Color.White;
+            this.twrKartyCzynnosciPictureBox.Enabled = false;
+            this.twrKartyCzynnosciPictureBox.Location = new System.Drawing.Point(142, 126);
+            this.twrKartyCzynnosciPictureBox.Name = "twrKartyCzynnosciPictureBox";
+            this.twrKartyCzynnosciPictureBox.Size = new System.Drawing.Size(32, 32);
+            this.twrKartyCzynnosciPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.twrKartyCzynnosciPictureBox.TabIndex = 7;
+            this.twrKartyCzynnosciPictureBox.TabStop = false;
+            // 
+            // kntAdresyPictureBox
+            // 
+            this.kntAdresyPictureBox.BackColor = System.Drawing.Color.White;
+            this.kntAdresyPictureBox.Enabled = false;
+            this.kntAdresyPictureBox.Location = new System.Drawing.Point(142, 88);
+            this.kntAdresyPictureBox.Name = "kntAdresyPictureBox";
+            this.kntAdresyPictureBox.Size = new System.Drawing.Size(32, 32);
+            this.kntAdresyPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.kntAdresyPictureBox.TabIndex = 8;
+            this.kntAdresyPictureBox.TabStop = false;
+            // 
+            // kntKartyPictureBox
+            // 
+            this.kntKartyPictureBox.BackColor = System.Drawing.Color.White;
+            this.kntKartyPictureBox.Enabled = false;
+            this.kntKartyPictureBox.Location = new System.Drawing.Point(142, 50);
+            this.kntKartyPictureBox.Name = "kntKartyPictureBox";
+            this.kntKartyPictureBox.Size = new System.Drawing.Size(32, 32);
+            this.kntKartyPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.kntKartyPictureBox.TabIndex = 9;
+            this.kntKartyPictureBox.TabStop = false;
+            // 
+            // operatorzyLabel
+            // 
+            this.operatorzyLabel.AutoSize = true;
+            this.operatorzyLabel.Location = new System.Drawing.Point(180, 22);
+            this.operatorzyLabel.Name = "operatorzyLabel";
+            this.operatorzyLabel.Size = new System.Drawing.Size(104, 13);
+            this.operatorzyLabel.TabIndex = 10;
+            this.operatorzyLabel.Text = "Gotowy do działania";
+            // 
+            // twrKartySkladnikiLabel
+            // 
+            this.twrKartySkladnikiLabel.AutoSize = true;
+            this.twrKartySkladnikiLabel.Location = new System.Drawing.Point(180, 174);
+            this.twrKartySkladnikiLabel.Name = "twrKartySkladnikiLabel";
+            this.twrKartySkladnikiLabel.Size = new System.Drawing.Size(104, 13);
+            this.twrKartySkladnikiLabel.TabIndex = 11;
+            this.twrKartySkladnikiLabel.Text = "Gotowy do działania";
+            // 
+            // kntAdresyLabel
+            // 
+            this.kntAdresyLabel.AutoSize = true;
+            this.kntAdresyLabel.Location = new System.Drawing.Point(180, 98);
+            this.kntAdresyLabel.Name = "kntAdresyLabel";
+            this.kntAdresyLabel.Size = new System.Drawing.Size(104, 13);
+            this.kntAdresyLabel.TabIndex = 12;
+            this.kntAdresyLabel.Text = "Gotowy do działania";
+            // 
+            // twrKartyCzynnosciLabel
+            // 
+            this.twrKartyCzynnosciLabel.AutoSize = true;
+            this.twrKartyCzynnosciLabel.Location = new System.Drawing.Point(180, 136);
+            this.twrKartyCzynnosciLabel.Name = "twrKartyCzynnosciLabel";
+            this.twrKartyCzynnosciLabel.Size = new System.Drawing.Size(104, 13);
+            this.twrKartyCzynnosciLabel.TabIndex = 13;
+            this.twrKartyCzynnosciLabel.Text = "Gotowy do działania";
+            // 
+            // kntKartyLabel
+            // 
+            this.kntKartyLabel.AutoSize = true;
+            this.kntKartyLabel.Location = new System.Drawing.Point(180, 60);
+            this.kntKartyLabel.Name = "kntKartyLabel";
+            this.kntKartyLabel.Size = new System.Drawing.Size(104, 13);
+            this.kntKartyLabel.TabIndex = 14;
+            this.kntKartyLabel.Text = "Gotowy do działania";
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(584, 361);
+            this.Controls.Add(this.kntKartyLabel);
+            this.Controls.Add(this.twrKartyCzynnosciLabel);
+            this.Controls.Add(this.kntAdresyLabel);
+            this.Controls.Add(this.twrKartySkladnikiLabel);
+            this.Controls.Add(this.operatorzyLabel);
+            this.Controls.Add(this.kntKartyPictureBox);
+            this.Controls.Add(this.kntAdresyPictureBox);
+            this.Controls.Add(this.twrKartyCzynnosciPictureBox);
+            this.Controls.Add(this.twrKartySkladnikiPictureBox);
+            this.Controls.Add(this.operatorzyPictureBox);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -76,7 +225,13 @@
             this.Name = "MainWindow";
             this.Text = "Aplikacja serwisowa";
             this.Shown += new System.EventHandler(this.Form1_Shown);
+            ((System.ComponentModel.ISupportInitialize)(this.operatorzyPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.twrKartySkladnikiPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.twrKartyCzynnosciPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kntAdresyPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.kntKartyPictureBox)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -85,6 +240,18 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PictureBox operatorzyPictureBox;
+        private System.Windows.Forms.PictureBox twrKartySkladnikiPictureBox;
+        private System.Windows.Forms.PictureBox twrKartyCzynnosciPictureBox;
+        private System.Windows.Forms.PictureBox kntAdresyPictureBox;
+        private System.Windows.Forms.PictureBox kntKartyPictureBox;
+        private System.Windows.Forms.Label operatorzyLabel;
+        private System.Windows.Forms.Label twrKartySkladnikiLabel;
+        private System.Windows.Forms.Label kntAdresyLabel;
+        private System.Windows.Forms.Label twrKartyCzynnosciLabel;
+        private System.Windows.Forms.Label kntKartyLabel;
     }
 }
 
--- AplikacjaSerwisowaKomp/MainWindow.cs
+++ AplikacjaSerwisowaKomp/MainWindow.cs
@@ -6,6 +6,7 @@ using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 using System.Xml;
 using System.Xml.Linq;
@@ -14,6 +15,7 @@ namespace AplikacjaSerwisowaKomp
 {
     public partial class MainWindow : Form
     {
+        private static MainWindow MainWindowForm;
         private static SqlConnection uchwytBD;
         private static SqlCommand polecenieSQL;
 
@@ -23,6 +25,7 @@ namespace AplikacjaSerwisowaKomp
         public MainWindow()
         {
             InitializeComponent();
+            MainWindowForm = this;
         }
 
         private void Form1_Shown(object sender, EventArgs e)
@@ -74,13 +77,16 @@ namespace AplikacjaSerwisowaKomp
             polecenieSQL.ExecuteNonQuery();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void operatorzyButton_Click(object sender, EventArgs e)
         {
-            pobierzOperatorow();
+            Thread t = new Thread(pobierzOperatorow);
+            t.Start();
         }
 
         private void pobierzOperatorow()
         {
+            AktualizujMainWindow(operatorzyLabel, "Odczyt z bazy danych...", operatorzyPictureBox, true);
+            
             DataTable pomDataTable = new DataTable();
             try
             {
@@ -91,11 +97,21 @@ namespace AplikacjaSerwisowaKomp
             catch(Exception exc)
             {
                 MessageBox.Show("Wystąpił błąd podczas wykonywania funkcji MainWindow.pobierzOperatorow() :\n" + exc.Message);
+
+                AktualizujMainWindow(operatorzyLabel, "Błąd...", operatorzyPictureBox, false);
             }
 
             if(pomDataTable.Rows.Count > 0)
             {
+                AktualizujMainWindow(operatorzyLabel, "Odczyt z bazy danych...", operatorzyPictureBox, true);
+
                 wygenerujPlikXML("operatorzy", "operator", pomDataTable);
+
+                AktualizujMainWindow(operatorzyLabel, "Wykonano...", operatorzyPictureBox, false);
+            }
+            else
+            {
+                AktualizujMainWindow(operatorzyLabel, "Brak wierszy do zapisania...", operatorzyPictureBox, false);
             }
         }
 
@@ -106,16 +122,19 @@ namespace AplikacjaSerwisowaKomp
             
             ds.Tables[0].TableName = pomocniczaNazwa;
             ds.WriteXml(glownaNazwa+".xml");
-            MessageBox.Show("Zobione");
+            // MessageBox.Show("Zobione");
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void kntKartyButton_Click(object sender, EventArgs e)
         {
-            pobierzKntKarty();
+            Thread t = new Thread(pobierzKntKarty);
+            t.Start();
         }
 
         private void pobierzKntKarty()
         {
+            AktualizujMainWindow(kntKartyLabel, "Odczyt z bazy danych...", kntKartyPictureBox, true);
+
             DataTable pomDataTable = new DataTable();
             try
             {
@@ -126,21 +145,34 @@ namespace AplikacjaSerwisowaKomp
             catch(Exception exc)
             {
                 MessageBox.Show("Wystąpił błąd podczas wykonywania funkcji MainWindow.pobierzKntKarty() :\n" + exc.Message);
+
+                AktualizujMainWindow(kntKartyLabel, "Błąd...", kntKartyPictureBox, false);
             }
 
             if(pomDataTable.Rows.Count > 0)
             {
+                AktualizujMainWindow(kntKartyLabel, "Tworzenie pliku...", kntKartyPictureBox, true);
+
                 wygenerujPlikXML("KntKarty", "KntKarta", pomDataTable);
+
+                AktualizujMainWindow(kntKartyLabel, "Wykonano...", kntKartyPictureBox, false);
+            }
+            else
+            {
+                AktualizujMainWindow(kntKartyLabel, "Brak wierszy do zapisania...", kntKartyPictureBox, false);
             }
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void kntAdresyButton_Click(object sender, EventArgs e)
         {
-            pobierzKntAdresy();
+            Thread t = new Thread(pobierzKntAdresy);
+            t.Start();
         }
 
         private void pobierzKntAdresy()
         {
+            AktualizujMainWindow(kntAdresyLabel, "Odczyt z bazy danych...", kntAdresyPictureBox, true);
+            
             DataTable pomDataTable = new DataTable();
             try
             {
@@ -151,12 +183,116 @@ namespace AplikacjaSerwisowaKomp
             catch(Exception exc)
             {
                 MessageBox.Show("Wystąpił błąd podczas wykonywania funkcji MainWindow.pobierzKntAdresy() :\n" + exc.Message);
+
+                AktualizujMainWindow(kntAdresyLabel, "Błąd...", kntAdresyPictureBox, false);
             }
 
             if(pomDataTable.Rows.Count > 0)
             {
+                AktualizujMainWindow(kntAdresyLabel, "Tworzenie pliku...", kntAdresyPictureBox, true);
+
                 wygenerujPlikXML("KntAdresy", "KntAdres", pomDataTable);
+
+                AktualizujMainWindow(kntAdresyLabel, "Wykonano...", kntAdresyPictureBox, false);
             }
+            else
+            {
+                AktualizujMainWindow(kntAdresyLabel, "Brak wierszy do zapisania...", kntAdresyPictureBox, false);
+            }
+        }
+
+        private void twrKartyCzynnosciButton_Click(object sender, EventArgs e)
+        {
+            Thread t = new Thread(pobierzTwrKartyCzynnosci);
+            t.Start();
+        }
+
+        private void pobierzTwrKartyCzynnosci()
+        {
+            AktualizujMainWindow(twrKartyCzynnosciLabel, "Odczyt z bazy danych...", twrKartyCzynnosciPictureBox, true);
+
+            DataTable pomDataTable = new DataTable();
+            try
+            {
+                String zapytanieString = "select twr_gidnumer, twr_kod, twr_typ, twr_nazwa, twr_nazwa1 from cdn.twrkarty where twr_typ = 4 order by twr_kod";
+                SqlDataAdapter da = zapytanie(zapytanieString);
+                da.Fill(pomDataTable);
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show("Wystąpił błąd podczas wykonywania funkcji MainWindow.pobierzTwrKartyCzynnosci() :\n" + exc.Message);
+
+                AktualizujMainWindow(twrKartyCzynnosciLabel, "Błąd...", twrKartyCzynnosciPictureBox, false);
+            }
+
+            if(pomDataTable.Rows.Count > 0)
+            {
+                AktualizujMainWindow(twrKartyCzynnosciLabel, "Tworzenie pliku...", twrKartyCzynnosciPictureBox, true);
+
+                wygenerujPlikXML("TwrKartyCzynnosci", "TwrKartyCzynnosc", pomDataTable);
+
+                AktualizujMainWindow(twrKartyCzynnosciLabel, "Wykonano...", twrKartyCzynnosciPictureBox, false);
+            }
+            else
+            {
+                AktualizujMainWindow(twrKartyCzynnosciLabel, "Brak wierszy do zapisania...", twrKartyCzynnosciPictureBox, false);
+            }
+        }
+
+        private void twrKartySkladnikiButton_Click(object sender, EventArgs e)
+        {
+            Thread t = new Thread(pobierzTwrKartySkladniki);
+            t.Start();
+        }
+
+        private void pobierzTwrKartySkladniki()
+        {
+            AktualizujMainWindow(twrKartySkladnikiLabel, "Odczyt z bazy danych...", twrKartySkladnikiPictureBox, true);
+
+            DataTable pomDataTable = new DataTable();
+            try
+            {
+                String zapytanieString = "select twr_gidnumer, twr_kod, twr_typ, twr_nazwa, twr_nazwa1  from cdn.twrkarty where twr_typ in (1,2) order by twr_kod";
+                SqlDataAdapter da = zapytanie(zapytanieString);
+                da.Fill(pomDataTable);
+            }
+            catch(Exception exc)
+            {
+                MessageBox.Show("Wystąpił błąd podczas wykonywania funkcji MainWindow.pobierzTwrKartyCzynnosci() :\n" + exc.Message);
+
+                AktualizujMainWindow(twrKartySkladnikiLabel, "Błąd...", twrKartySkladnikiPictureBox, false);
+            }
+
+            if(pomDataTable.Rows.Count > 0)
+            {
+                AktualizujMainWindow(twrKartySkladnikiLabel, "Tworzenie pliku...", twrKartySkladnikiPictureBox, true);
+
+                wygenerujPlikXML("TwrKartySkladniki", "TwrKartySkladnik", pomDataTable);
+
+                AktualizujMainWindow(twrKartySkladnikiLabel, "Wykonano...", twrKartySkladnikiPictureBox, false);
+            }
+            else
+            {
+                AktualizujMainWindow(twrKartySkladnikiLabel, "Brak wierszy do zapisania...", twrKartySkladnikiPictureBox, false);
+            }
+        }
+
+        private static void AktualizujMainWindow(Label pomLabel, String tekst, PictureBox pomPictureBox, Boolean status)
+        {
+            MainWindowForm.BeginInvoke(new EventHandler(delegate
+            {
+                if(status)
+                {
+                    pomPictureBox.Enabled = status;
+                    pomPictureBox.Image = AplikacjaSerwisowaKomp.Properties.Resources.animatedCircle;
+                }
+                else
+                {
+                    pomPictureBox.Enabled = status;
+                    pomPictureBox.Image = null;
+                }
+                pomLabel.Text = tekst;
+            }));
         }
     }
 }
